9906ffe8261eb9e504558738cc3973d95d52f120
sabs <dziedziochs@gmail.com>
2024-01-24 04:51:03 +0100

    Compose a viable data structures for efficient and optimized rendering

---
+++ src/data.js
@@ -0,0 +1,199 @@
+const data = {
+  category1: {
+    label: "Category 1",
+    url: "category-1",
+    description: "Description of Category 1",
+    products: [
+      {
+        id: 1,
+        url: "product-1",
+        name: "Product 1",
+        category: "Category 1",
+        brand: "Brand 1",
+        colors: ["Red", "Blue", "Green"],
+        description: "Description of Product 1",
+        releaseDate: "2021-01-01",
+        features: ["Feature 1", "Feature 2", "Feature 3"],
+        thumbnail: "./assets/images/products/gameboy.jpg",
+        images: [
+          "./assets/images/products/gameboy.jpg",
+          "./assets/images/products/gameboy.jpg",
+        ],
+        availability: true,
+        price: 9.99,
+        discount: 0.2,
+        rating: 4,
+        reviews: [
+          { user: "User 1", comment: "Review 1" },
+          { user: "User 2", comment: "Review 2" },
+        ],
+        tags: ["Vintage audio gear"],
+        featured: true,
+      },
+      {
+        id: 2,
+        url: "product-1",
+        name: "Product 1",
+        category: "Category 1",
+        brand: "Brand 1",
+        colors: ["Red", "Blue", "Green"],
+        description: "Description of Product 1",
+        releaseDate: "2021-01-01",
+        features: ["Feature 1", "Feature 2", "Feature 3"],
+        thumbnail: "./assets/images/products/gameboy.jpg",
+        images: ["./assets/images/products/gameboy.jpg"],
+        availability: true,
+        price: 9.99,
+        discount: 0.2,
+        rating: 4,
+        reviews: [
+          { user: "User 1", comment: "Review 1" },
+          { user: "User 2", comment: "Review 2" },
+        ],
+        tags: ["Tag 1", "Tag 2"],
+        featured: false,
+      },
+      {
+        id: 3,
+        url: "product-1",
+        name: "Product 1",
+        category: "Category 1",
+        brand: "Brand 1",
+        colors: ["Red", "Blue", "Green"],
+        description: "Description of Product 1",
+        releaseDate: "2021-01-01",
+        features: ["Feature 1", "Feature 2", "Feature 3"],
+        thumbnail: "./assets/images/products/gameboy.jpg",
+        images: [
+          "./assets/images/products/gameboy.jpg",
+          "./assets/images/products/gameboy.jpg",
+        ],
+        availability: true,
+        price: 9.99,
+        discount: 0.2,
+        rating: 4,
+        reviews: [
+          { user: "User 1", comment: "Review 1" },
+          { user: "User 2", comment: "Review 2" },
+        ],
+        tags: ["summer vibes", "90's charm"],
+        featured: false,
+      },
+    ],
+  },
+  category2: {
+    label: "Category 2",
+    url: "category-2",
+    description: "Description of Category 2",
+    products: [
+      {
+        id: 4,
+        url: "product-1",
+        name: "Product 1",
+        category: "Category 2",
+        brand: "Brand 1",
+        colors: ["Red", "Blue", "Green"],
+        description: "Description of Product 1",
+        releaseDate: "2021-01-01",
+        features: ["Feature 1", "Feature 2", "Feature 3"],
+        thumbnail: "./assets/images/products/gameboy.jpg",
+        images: [
+          "./assets/images/products/gameboy.jpg",
+          "./assets/images/products/gameboy.jpg",
+        ],
+        availability: true,
+        price: 9.99,
+        discount: 0.2,
+        rating: 4,
+        reviews: [
+          { user: "User 1", comment: "Review 1" },
+          { user: "User 2", comment: "Review 2" },
+        ],
+        tags: ["Vintage audio gear", "Sporting Goods"],
+        featured: true,
+      },
+    ],
+  },
+};
+
+const categories = Object.keys(data).reduce((result, categoryKey) => {
+  const category = data[categoryKey];
+  const productCount = category.products ? category.products.length : 0;
+
+  const categoryInfo = {
+    label: category.label,
+    url: category.url,
+    description: category.description,
+    productCount: productCount,
+  };
+
+  result[categoryKey] = categoryInfo;
+  return result;
+}, {});
+
+const tags = Object.keys(data).reduce((result, categoryKey) => {
+  const category = data[categoryKey];
+
+  category.products.forEach((product) => {
+    product.tags.forEach((tag) => {
+      const cleanTag = tag.toLowerCase().replace(/[^a-z0-9]+/g, "-");
+      if (!result[cleanTag]) {
+        result[cleanTag] = {
+          label: tag,
+          url: cleanTag,
+          productCount: 0,
+          productUrls: "",
+          productIds: [],
+        };
+      }
+
+      result[cleanTag].productCount++;
+      result[cleanTag].productUrls +=
+        (result[cleanTag].productUrls ? "," : "") + product.url.toLowerCase();
+      result[cleanTag].productIds.push(product.id);
+    });
+  });
+
+  return result;
+}, {});
+
+const featuredProducts = Object.keys(data).reduce((result, categoryKey) => {
+  const category = data[categoryKey];
+
+  category.products.forEach((product) => {
+    if (product.featured) {
+      const {
+        id,
+        url,
+        name,
+        brand,
+        category: productCategory,
+        price,
+        discount,
+      } = product;
+      result.push({
+        id,
+        url,
+        name,
+        brand,
+        category: productCategory,
+        price,
+        discount,
+      });
+    }
+  });
+
+  return result;
+}, []);
+
+export { data, categories, featuredProducts, tags };
+
+const cats = [
+  "cameras",
+  "eyeglasses",
+  "t-shirts",
+  "shoes",
+  "Audio Equipment",
+  "Gaming Hardware",
+  "sports equipment",
+];
